650a188694eb2288577bc7d1baba6a539ab5b1a2
leejh <leejh@biztechpartners.co.kr>
2019-04-24 15:11:19 +0900

    Vue CLI 3.0 시작하기 - 03

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ count }}</p>
+    <button @click="count ++">추가</button>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
+  data() {
     return {
-      //
-    }
+      title: "안녕하세요.",
+      count: 1
+    };
   }
-}
+};
 </script>
