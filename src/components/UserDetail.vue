308e495e2ad0da8608ae69e0262047421ee6ee57
leejh <leejh@biztechpartners.co.kr>
2019-04-24 17:08:45 +0900

    Vue CLI 3.0 시작하기 - 06
    부모 > 자식으로 값 전달

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,14 +2,32 @@
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+    <v-btn @click="switchName()">이름 변경</v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["nameOfChild"],
+  props: {
+    nameOfChild: {
+      type: String,
+      // required: true,
+      default: "기본값입니다."
+    }
+  },
   data() {
     return {};
+  },
+  computed: {
+    sayHello() {
+      return this.nameOfChild + "안녕하세요~";
+    }
+  },
+  methods: {
+    switchName() {
+      this.nameOfChild = "컴퓨터";
+    }
   }
 };
 </script>
